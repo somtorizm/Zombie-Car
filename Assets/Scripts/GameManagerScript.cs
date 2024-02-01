65c704256bd8593d255a0face23e2c2f814cd55d
victorsomto <ezinwavictor25@gmail.com>
2024-02-01 09:26:16 +0100

    add pause when reached required miles

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -8,6 +8,9 @@ public class GameManagerScript : MonoBehaviour
 
     public float totalDistanceInMiles = 10f;
     private float remainingDistance;
+    private bool isPaused = false;
+    private bool reachedDestination = false;
+    private bool zombieIsDisable = false;
 
     [SerializeField]
     public TextMeshProUGUI distanceText;
@@ -15,6 +18,9 @@ public class GameManagerScript : MonoBehaviour
     [SerializeField]
     public float countdownDurationInSeconds = 60f; 
     private float speed;
+
+    [SerializeField]
+    private GameObject zombieSpawner;
 
     void Start()
     {
@@ -34,11 +40,53 @@ public class GameManagerScript : MonoBehaviour
         {
             remainingDistance -= speed * Time.deltaTime; // Update remaining distance
             distanceText.text = "Distance: " + remainingDistance.ToString("F1") + " miles";
+            DisableZombieSpawn();
 
         }
         else
         {
-            remainingDistance = 0f; // Ensure the distance doesn't go below zero
+            remainingDistance = 0f;
+            if(!reachedDestination)
+            {
+                reachedDestination = true;
+                TogglePause();
+            }
+           
+        }
+    }
+
+    void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            PauseGame();
+        }
+        else
+        {
+            ResumeGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0f; // Set time scale to 0 to pause the game
+        // You can also add additional pause-related functionality here (e.g., showing a pause menu)
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1f; // Set time scale back to 1 to resume the game
+        // You can also add additional resume-related functionality here
+    }
+
+    void DisableZombieSpawn()
+    {
+        if(remainingDistance < 4f && !zombieIsDisable)
+        {
+            zombieIsDisable = true;
+            zombieSpawner.SetActive(false);
         }
     }
 
